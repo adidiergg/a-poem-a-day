3a857690ac29527d6a7f90bf53ad3eb31bc728ce
alanddgg <alanddgg@alanddgg.alanddgg>
2024-06-29 01:25:05 -0600

    fix styles post

--- src/app/_components/post_poem.tsx
+++ src/app/_components/post_poem.tsx
@@ -14,8 +14,8 @@ export const PostPoem = ({ poem }: PoemProps) => {
 
   return (
     <>
-      <div className="flex max-h-svh  w-full rounded-lg bg-background shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]  lg:w-4/5 xl:w-4/5 ">
-        <div className="flex basis-full flex-col justify-between  p-16 gap-4">
+      <div className="z-0 flex h-fit w-full rounded-lg bg-background shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]  lg:w-4/5 xl:w-4/5 ">
+        <div className="flex basis-full flex-col justify-between gap-4 p-8 md:px-12">
           <h1 className="text-md  text-right italic text-primary/90 lg:text-lg">
             {createdAt.toLocaleDateString("es-ES", {
               year: "numeric",
@@ -27,10 +27,10 @@ export const PostPoem = ({ poem }: PoemProps) => {
           <h1 className="mt-1 text-center text-2xl font-bold text-primary/90 lg:text-2xl">
             {title}
           </h1>
-          <div className="border-l-4 border-primary/90 px-4 ">
+          <div className="border-l-4 border-primary/90 px-4">
             <Markdown
               className={cn(
-                "truncate mb-4 mt-1 text-center text-lg text-primary/80",
+                "mb-4 mt-1 text-center text-lg text-primary/80 lg:text-lg",
                 fontGaramond.className,
               )}
             >
