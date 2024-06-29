9c1635d23de7fd7783e8ef171826cbeb6a19642c
alanddgg <alanddgg@alanddgg.alanddgg>
2024-06-29 01:13:11 -0600

    styles post

--- src/app/(home)/poems/layout.tsx
+++ src/app/(home)/poems/layout.tsx
@@ -6,7 +6,9 @@ export default function PoemLayouy({ children }: PoemLayoutProps) {
   return (
     <>
       <div
-        className=" relative flex min-h-screen justify-center  p-8 py-16  "
+        className=" relative flex h-screen p-8 justify-center 
+      bg-primary   before:pointer-events-none before:absolute before:inset-0 before:block before:h-full 
+      before:w-full before:bg-[url('/images/background-pattern.svg')] before:bg-repeat before:opacity-5 "
       >
         {children}
       </div>
