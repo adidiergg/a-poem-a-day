4ed0ff0c62733c973cdcec6a82ffc2b821fdf2be
alanddgg <alanddgg@alanddgg.alanddgg>
2024-07-11 16:41:48 -0600

    update skeleton

--- src/components/skeleton_poem_post.tsx
+++ src/components/skeleton_poem_post.tsx
@@ -2,6 +2,9 @@ import { Skeleton } from "~/components/ui/skeleton"
 
 export function SkeletonPoemPost(){
     return (
-        <Skeleton className="z-0 flex  w-full h-64 flex-col rounded-lg bg-background/40 p-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5"/>
+        <Skeleton className="z-0 flex  w-full gap-2 h-64 flex-col rounded-lg bg-background/40 p-4 py-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5">
+             <div className="h-48 rounded-sm w-full bg-primary/40"> </div>
+             <div className="h-16 rounded-sm w-full bg-primary/40"> </div>
+        </Skeleton>
     );
 }
--- src/components/skeleton_post_home.tsx
+++ src/components/skeleton_post_home.tsx
@@ -3,11 +3,21 @@ import { Skeleton } from "~/components/ui/skeleton"
 export function SkeletonPostsHome(){
     return (
         <>
-        <Skeleton className="z-0 flex  w-full h-32 flex-col rounded-lg bg-background/40 p-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5"/>
-        <Skeleton className="z-0 flex  w-full h-32 flex-col rounded-lg bg-background/40 p-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5"/>
-        <Skeleton className="z-0 flex  w-full h-32 flex-col rounded-lg bg-background/40 p-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5"/>
-        <Skeleton className="z-0 flex  w-full h-32 flex-col rounded-lg bg-background/40 p-8  shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5"/>
-
+        <Skeleton className="z-0 flex  w-full h-32 flex-col items-end gap-2 rounded-lg bg-background/40 p-4 py-8 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5">
+          <div className="h-8 rounded-sm w-1/2 bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+        </Skeleton>
+        <Skeleton className="z-0 flex  w-full h-32 flex-col items-end gap-2 rounded-lg bg-background/40 p-4 py-8 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5">
+          <div className="h-8 rounded-sm w-1/2 bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+        </Skeleton>
+        <Skeleton className="z-0 flex  w-full h-32 flex-col items-end gap-2 rounded-lg bg-background/40 p-4 py-8 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] md:px-12 lg:w-4/5 xl:w-4/5">
+          <div className="h-8 rounded-sm w-1/2 bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+          <div className="h-8 rounded-sm w-full bg-primary/40"> </div>
+        </Skeleton>
         </>
     );
 }
