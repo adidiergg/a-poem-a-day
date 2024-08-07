5f923d86600c149fbfa41746c7f9f8974af14e3e
alanddgg <alandidiergon@gmail.com>
2024-08-07 16:48:20 -0600

    suspense

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,17 +1,11 @@
 import { Posts } from "~/app/_components/posts";
 import { Suspense } from "react";
 
-
-
-
-
 export  default function ExplorePage() {
 
     return (
-        <>
-         <Suspense fallback={<div>Loading...</div>}>
+        <Suspense>
             <Posts />
-         </Suspense>
-        </>
+        </Suspense>
     );
 }
