9bcd63b6f415256874422f101889e97573575175
alanddgg <alandidiergon@gmail.com>
2024-08-07 20:17:06 -0600

    pagination

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -170,7 +170,6 @@ export const Icons = {
       {...props}
     >
       <polygon
-        fill-rule="evenodd"
         points="31,38.32 13.391,21 31,3.68 28.279,1 8,21.01 28.279,41 "
       />
     </svg>
@@ -184,7 +183,6 @@ export const Icons = {
       {...props}
     >
       <polygon
-        fill-rule="evenodd"
         points="11,38.32 28.609,21 11,3.68 13.72,1 34,21.01 13.72,41 "
       />
     </svg>
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -3,6 +3,7 @@ import { useCallback } from "react";
 import Link from "next/link";
 import next from "next";
 import { Icons } from "./icons";
+import { cn } from "~/lib/utils";
 
 interface PaginationProps {
   page: number;
@@ -25,7 +26,12 @@ export const Pagination = ({ totalPages, page }: PaginationProps) => {
     },
     [searchParams],
   );
-  console.log(prevPage, page === 1, nextPage, totalPages);
+
+  console.log(
+    totalPages === page || totalPages - page < 5 ? page - 5 : page,
+    totalPages === page || totalPages - page < 5 ? page : page + 5,
+  );
+
   return (
     <div className="flex w-full flex-row items-center justify-center gap-1">
       {page !== 1 && (
@@ -48,33 +54,35 @@ export const Pagination = ({ totalPages, page }: PaginationProps) => {
           </Link>
         </>
       )}
+      { page !== totalPages-1 && (
       <div className="flex flex-row gap-1">
-        {Array.from({ length: totalPages }, (_, i) => i)
+        {Array.from({ length: totalPages }, (_, i) => i + 1)
+          .slice(
+            totalPages === page ? page - 4 : page-1,
+            totalPages === page ? page : page + 4,
+          )
           .map((page) => {
             return (
               <Link key={page} href={`${pathname}?${changePage(page)}`}>
                 <div
-                  className={
-                    "flex h-10 w-10 items-center justify-center rounded-md  bg-background fill-primary p-2 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]"
+                  className={cn(
+                    "flex h-10 w-10 items-center justify-center rounded-md  bg-background fill-primary p-2 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]")
                   }
                 >
                   <span className="font-medium">{page}</span>
                 </div>
               </Link>
             );
-          })
-          .slice(
-            totalPages === page || totalPages - page < 5 ? page - 5 : page,
-            totalPages === page || totalPages - page < 5 ? page + 1 : page + 5,
-          )}
+          })}
       </div>
+      )}
 
       {page !== totalPages && (
         <>
           <Link href={`${pathname}?${changePage(totalPages)}`}>
             <div
               className={
-                "flex h-10 w-10 items-center justify-center rounded-md  bg-background fill-primary p-2 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]"
+                "flex h-10 w-10 items-center justify-center rounded-md  bg-background fill-primary p-3 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]"
               }
             >
               <span className="font-medium">{totalPages}</span>
