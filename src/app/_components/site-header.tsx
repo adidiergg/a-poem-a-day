3648d156a8d43a327790e3a849bf3aac58d42848
alanddgg <alandidiergon@gmail.com>
2024-08-10 18:48:39 -0600

    navbar styles

--- src/app/_components/site-header.tsx
+++ src/app/_components/site-header.tsx
@@ -25,7 +25,7 @@ export function SiteHeader() {
 
 
   return (
-    <header className={cn("fixed top-0 z-10 w-full p-1 px-3 shadow-[rgba(0,_0,_0,_0.24)_0px_7px_8px]",styles,"bg-gradient-to-t from-primary/70 to-primary")}>
+    <header className={cn("fixed top-0 z-10 w-full p-1 px-3 shadow-[rgba(0,_0,_0,_0.24)_0px_7px_8px]","bg-gradient-to-t from-primary/70 to-primary")}>
       <div className=" flex h-12  items-center ">
         <div className="flex flex-1 items-center justify-between">
           <div className="">
